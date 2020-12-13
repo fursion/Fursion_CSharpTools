c1c1ac0ced2cdc41d3c2253d08c24a223b8fac9a
fursion <fursion@fursion.cn>
2020-12-13 23:45:39 +0800

    添加项目文件。

---
+++ Fursion_CSharpTools/ClassTools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fursion_CSharpTools
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Singleton<T>where T:new()
+    {
+        public T instance;
+        public T GetInstance()
+        {
+            if (instance == null)
+                instance = new T();
+            return instance;
+        }
+    }
+
+}
